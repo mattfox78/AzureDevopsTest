621a43bd0d3bb8a23148890a8c8b0372c3ed2a35
Jordan Cavins <Jordan.Cavins@ClaytonHomes.com>
2023-03-23 14:35:52 -0400

    add dummy unit tests

--- WeatherForecast.Tests/UnitTest1.cs
+++ WeatherForecast.Tests/UnitTest1.cs
@@ -1,10 +1,43 @@
-namespace WeatherForecast.Tests;
+using WeatherForecast.Api.Models;
 
-public class UnitTest1
+namespace WeatherForecast.Tests
 {
-    [Fact]
-    public void Test1()
+    public class Tests
     {
+        public IWeatherClient WeatherClient { get; set; }
+        [SetUp]
+        public void Setup()
+        {
+            WeatherClient = A.Fake<IWeatherClient>();
+        }
 
+        [Test]
+        public void Test1()
+        {
+            A.CallTo(() => WeatherClient.GetCurrentWeatherForCity(A<string>.Ignored)).Returns(new Api.Models.WeatherResponse());
+
+            var response = WeatherClient.GetCurrentWeatherForCity("knoxville").Result;
+            Assert.That(response, Is.Not.Null);
+            Assert.That(response, Is.TypeOf<Api.Models.WeatherResponse>());
+
+            var response2 = WeatherClient.GetCurrentWeatherForCity("maryville").Result;
+            Assert.That(response2, Is.Not.Null);
+            Assert.That(response2, Is.TypeOf<Api.Models.WeatherResponse>());
+
+            A.CallTo(() => WeatherClient.GetCurrentWeatherForCity(A<string>.Ignored)).MustHaveHappenedTwiceExactly();
+        }
+
+        [Test]
+        public void Test2()
+        {
+            Assert.That(WeatherClient, Is.Not.Null);
+            A.CallTo(() => WeatherClient.GetCurrentWeatherForCity(A<string>.Ignored)).Returns(new Api.Models.WeatherResponse());
+
+            var response = WeatherClient.GetCurrentWeatherForCity("knoxville").Result;
+            Assert.That(response, Is.Not.Null);
+            Assert.That(response, Is.TypeOf<Api.Models.WeatherResponse>());
+
+            A.CallTo(() => WeatherClient.GetCurrentWeatherForCity(A<string>.Ignored)).MustHaveHappenedOnceExactly();
+        }
     }
 }
--- WeatherForecast.Tests/Usings.cs
+++ WeatherForecast.Tests/Usings.cs
@@ -1,1 +1,3 @@
-global using Xunit;
+global using NUnit.Framework;
+global using FakeItEasy;
+global using WeatherForecast.Api;
